2ab6e593ffcb0b83397a51b6d9e190511becb43e
OscarGlo <oscarglo@free.fr>
2020-10-25 16:21:16 +0100

    context menu wip

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+String.prototype.searchLast = function (searcher) {
+    let index = -1, next = -1;
+    do {
+        index += next + 1;
+        next = this.substr(index + 1).search(searcher);
+    } while (next !== -1);
+    return index;
+};
+
 window.addEventListener("load", () => {
     document.querySelectorAll("#text p").forEach(p => {
-        p.innerHTML = "<span>" +
-            p.innerHTML.replaceAll(/(?<!^\s*)\n(?!\s*$)/g, "</span>\n<span>")
-            + "</span>";
+        p.innerHTML = p.innerHTML.replace(/(?<=\s*)([a-zéèàêî'-]+)(?=\s*)/gi, "<span class='word'>$1</span>");
+        p.innerHTML = p.innerHTML.replace(/\s*(.+)\s*/gi, "<span class='unit'>$1</span>\n");
     });
 
     let text = document.getElementById("text");
@@ -22,5 +30,48 @@ window.addEventListener("load", () => {
             if (remClass != null) text.classList.remove(remClass);
             if (addClass != null) text.classList.add(addClass);
         });
+    });
+
+    text.addEventListener("scroll", evt => {
+        let contHeight = document.getElementById("text-container").clientHeight;
+        document.getElementById("gradient-start").style.opacity = "" + Math.min(text.scrollTop / 20, 1);
+        document.getElementById("gradient-end").style.opacity = "" + Math.min((text.scrollHeight - contHeight - text.scrollTop + 20) / 20, 1);
+    });
+
+    let contextMenu = document.getElementById("contextMenu");
+
+    let selected = null, selectedAt = 0;
+
+    document.querySelectorAll("#text .word").forEach(span => {
+        span.addEventListener("click", evt => {
+            evt.stopPropagation();
+
+            if (selected != null)
+                selected.classList.remove("selected");
+
+            if (Date.now() - selectedAt < 800 || selected === span || selected === span.parentNode) {
+                selected = (selected === span ? span.parentNode : span);
+            } else {
+                selected = (selected === span || selected === span.parentNode ? (selected == null ? span : null) : span);
+            }
+
+            contextMenu.style.display = selected != null ? "" : "none";
+
+            if (selected != null) {
+                contextMenu.offsetTop = selected.offsetTop;
+                contextMenu.offsetLeft = selected.offsetLeft;
+                selected.classList.add("selected");
+                selectedAt = Date.now();
+            } else {
+                selectedAt = 0;
+            }
+        });
+    });
+
+    text.addEventListener("click", () => {
+        if (selected != null) {
+            selected.classList.remove("selected");
+            selected = null;
+        }
     });
 });
